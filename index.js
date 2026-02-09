5f1cd6cc2fdd87a742723b82d04c8248a85e2ad0
vineeta <220020057@iitdh.ac.in>
2026-02-10 02:35:29 +0530

    Basic notification service steup done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+
+
+const app = express();
+env.config();
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(process.env.PORT, async () => {
+  console.log("Notification server started")
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    console.log("Successfully connected to mongoose")
+  } catch (error) {
+    console.log(error);
+  }
+})
