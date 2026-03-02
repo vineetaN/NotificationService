03a4c0c38417eb3f88eb446f167ec26cf20a6459
vineeta <220020057@iitdh.ac.in>
2026-03-02 23:29:48 +0530

    Fix: small changes for deployment

--- services/email.service.js
+++ services/email.service.js
@@ -1,36 +1,19 @@
-// const nodemailer = require("nodemailer");
-
-
-
-
-
-// const mailer = (userId , password , mailData) => {
-//   return nodemailer.createTransport({
-//    service : "Gmail",
-//    auth : {
-//     user : userId,
-//     pass : password
-//    }
-// });
-   
-// }
-
-
-// module.exports = mailer
-
 const nodemailer = require("nodemailer");
 
-const mailer = (userId, password, mailData) => {
-  return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    auth: {
-      user: userId,
-      pass: password
-    }
-  });
-};
 
-module.exports = mailer;
+
+
+
+const mailer = (userId , password , mailData) => {
+  return nodemailer.createTransport({
+   service : "Gmail",
+   auth : {
+    user : userId,
+    pass : password
+   }
+});
+   
+}
+
+
+module.exports = mailer
